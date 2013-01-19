68788a2d6ae08979ac5d246727cc3de03bb76ff8
sensey <ssmartt@gmail.com>
2013-01-19 17:05:14 +0100

    Add 2 methods, display flag and change button icon

--- extension.js
+++ extension.js
@@ -18,55 +18,46 @@ DisplayKeyboardLayout.prototype = {
 
     meta: 	null,
     config: null,
-    icon: null,
     button: null,
 
     _init: function (meta) {
 
       this.meta   = meta;
       this.config = Gkbd.Configuration.get();
-      
-      this.config.connect('changed',        Lang.bind(this, this.doDisplayLayout));
-      this.config.connect('group-changed',  Lang.bind(this, this.doDisplayLayout));
-      
       this.button = new St.Bin({ style_class: 'panel-button' });
       
-      this.doDisplayLayout();
+      this.config.connect('changed',        Lang.bind(this, this._displayLayoutIcon));
+      this.config.connect('group-changed',  Lang.bind(this, this._displayLayoutIcon));
+      
+      this.config.connect('changed',        Lang.bind(this, this._displayLayoutFlag));
+      this.config.connect('group-changed',  Lang.bind(this, this._displayLayoutFlag));
+      
+      this._displayLayoutIcon();
     },
 
     getIconImage: function () {
-      
       var group_id   = this.config.get_current_group();
       var group_name = this.config.get_group_name(group_id);
-      
       return Gio.icon_new_for_string(this.meta.path + "/icons/" + group_name + ".svg");
     },
-    
-    doDisplayLayout: function () {
 
+    getPositionX: function (width) {
+      return Math.floor(Main.layoutManager.primaryMonitor.width / 2 - width / 2);
+    },
+    
+    getPositionY: function (height) {
+      return Math.floor(Main.layoutManager.primaryMonitor.height / 2 - height / 2);
+    },
+    
+    _displayLayoutFlag: function () {
       var flag = new St.Icon({
         style_class: 'display-keyboard-layout-image',
         gicon: this.getIconImage(),
         icon_size: 300,
+        x: this.getPositionX(300),
+        y: this.getPositionY(300),
       });
-
-      var icon = new St.Icon({
-        gicon: this.getIconImage(),
-        icon_size: 25,
-        style_class: 'system-status-icon' 
-      });
-      
-      this.button.set_child(icon);
-      
       Main.uiGroup.add_actor(flag);
-
-      let monitor = Main.layoutManager.primaryMonitor;
-
-      var x = Math.floor(monitor.width / 2 - flag.width / 2);
-      var y = Math.floor(monitor.height / 2 - flag.height / 2); 
-
-      flag.set_position(x, y);
-
       Tweener.addTween(flag, { 
         time: 1,
         opacity: 0,
@@ -77,10 +68,18 @@ DisplayKeyboardLayout.prototype = {
       });				
     },
 
+    _displayLayoutIcon: function () {
+      var icon = new St.Icon({
+        gicon: this.getIconImage(),
+        icon_size: 25,
+        style_class: 'system-status-icon' 
+      });
+      this.button.set_child(icon);
+    },
+    
     enable: function () {
       Main.panel._rightBox.insert_child_at_index(this.button, 0);      
       this.config.start_listen();
-      
     },
 
     disable: function () {

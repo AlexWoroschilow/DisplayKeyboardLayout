6c32abf8b4afa06bc5a7aa105ced8ad3ae49b64c
sensey <ssmartt@gmail.com>
2013-01-19 16:38:32 +0100

    Add panel icon with flags, remove png flags

--- extension.js
+++ extension.js
@@ -18,54 +18,73 @@ DisplayKeyboardLayout.prototype = {
 
     meta: 	null,
     config: null,
+    icon: null,
+    button: null,
 
     _init: function (meta) {
-      
+
       this.meta   = meta;
       this.config = Gkbd.Configuration.get();
       
       this.config.connect('changed',        Lang.bind(this, this.doDisplayLayout));
       this.config.connect('group-changed',  Lang.bind(this, this.doDisplayLayout));
+      
+      this.button = new St.Bin({ style_class: 'panel-button' });
+      
+      this.doDisplayLayout();
     },
 
-    doDisplayLayout: function (display, screen, window, binding) {
-
+    getIconImage: function () {
+      
       var group_id   = this.config.get_current_group();
       var group_name = this.config.get_group_name(group_id);
+      
+      return Gio.icon_new_for_string(this.meta.path + "/icons/" + group_name + ".svg");
+    },
+    
+    doDisplayLayout: function () {
 
-      var text = new St.Icon({
-        gicon: Gio.icon_new_for_string(this.meta.path + "/icons/"+group_name+".png"),
+      var flag = new St.Icon({
         style_class: 'display-keyboard-layout-image',
+        gicon: this.getIconImage(),
         icon_size: 300,
       });
 
-      Main.uiGroup.add_actor(text);
+      var icon = new St.Icon({
+        gicon: this.getIconImage(),
+        icon_size: 25,
+        style_class: 'system-status-icon' 
+      });
+      
+      this.button.set_child(icon);
+      
+      Main.uiGroup.add_actor(flag);
 
       let monitor = Main.layoutManager.primaryMonitor;
 
-      var x = Math.floor(monitor.width / 2 - text.width / 2);
-      var y = Math.floor(monitor.height / 2 - text.height / 2); 
+      var x = Math.floor(monitor.width / 2 - flag.width / 2);
+      var y = Math.floor(monitor.height / 2 - flag.height / 2); 
 
-      text.set_position(x, y);
+      flag.set_position(x, y);
 
-      Tweener.addTween(text, { 
+      Tweener.addTween(flag, { 
         time: 1,
         opacity: 0,
         transition: 'easeOutQuad',
         onComplete: function () {
-          Main.uiGroup.remove_actor(text);
+          Main.uiGroup.remove_actor(flag);
         }
       });				
     },
 
     enable: function () {
-      
+      Main.panel._rightBox.insert_child_at_index(this.button, 0);      
       this.config.start_listen();
       
     },
 
     disable: function () {
-      
+      Main.panel._rightBox.remove_child(this.button);      
       this.config.stop_listen();
     }
 };
